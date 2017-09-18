f71908b4daaa7a9e14a37fcd8826bf93ccab2220
victoriagonzalez58 <Victoria.Gonzalez-Calderon@gompersstudent.org>
2017-09-18 14:51:55 -0700

    Add files via upload

--- src/conditionals/TheRiddler.java
+++ src/conditionals/TheRiddler.java
@@ -3,13 +3,24 @@
 
 package conditionals;
 
+import javax.swing.JOptionPane;
+
 public class TheRiddler {
 
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-
+int score=0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
+String ans = JOptionPane.showInputDialog(null, "What can travel around the world while staying in a corner?");
+String anw = ("a stamp");
+if(ans.equalsIgnoreCase(anw)){
+	JOptionPane.showMessageDialog(null, "correct");
+}else{
+	JOptionPane.showMessageDialog(null, "wrong \n it's a stamp");
+			}
+
+}
 
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
 
@@ -20,5 +31,5 @@ public class TheRiddler {
 		// 2. Make a pop up to show the score.
 		
 	}
-}
+
 
