2304e99044217dabaa6701bee9a860f6021af29f
victoriagonzalez58 <Victoria.Gonzalez-Calderon@gompersstudent.org>
2017-10-09 13:41:53 -0700

    Add files via upload

---
+++ src/conditionals/nopotato4.java
@@ -0,0 +1,21 @@
+package conditionals;
+
+public class nopotato4 {
+public static void main(String[] args) {
+
+	for (int i=1; i<=9;i++){
+		if (i==4){
+			System.out.print("4 \n");
+		}else{
+			if (i==7){
+				System.out.print("More! \n");
+			}else{
+		
+		System.out.println( i +" Potato");
+		}
+			
+		}
+	}
+
+}
+}
